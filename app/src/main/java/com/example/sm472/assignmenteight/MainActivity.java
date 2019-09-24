994c96b1178ec8b107d29cc2cc3fcc3449bae1c1
sm472 <>
2019-09-25 10:10:40 +1200

    New Project (Deleted .iml)

---
+++ app/src/main/java/com/example/sm472/assignmenteight/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sm472.assignmenteight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
