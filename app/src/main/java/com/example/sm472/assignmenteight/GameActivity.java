2a7ad1febbdea5ff0ea39d83b2cc2c3a3c0255e8
sm472 <>
2019-09-25 11:03:34 +1200

    Added code to gameActivity and MainActivity to open as fullscreens

--- app/src/main/java/com/example/sm472/assignmenteight/GameActivity.java
+++ app/src/main/java/com/example/sm472/assignmenteight/GameActivity.java
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends  AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
 
         //Hide actionbar
         ActionBar actionBar = (ActionBar) getSupportActionBar();
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         //set fullscreen sticky immersive
         int uioptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         getWindow().getDecorView().setSystemUiVisibility(uioptions);
     }
 }
