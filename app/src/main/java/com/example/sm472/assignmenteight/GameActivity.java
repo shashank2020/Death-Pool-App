dc35280fa2af556d7cd7ff762c10d8a6f0b31bcb
sm472 <>
2019-09-25 13:48:37 +1200

    Added onClick code to play button which opens game activity

--- app/src/main/java/com/example/sm472/assignmenteight/GameActivity.java
+++ app/src/main/java/com/example/sm472/assignmenteight/GameActivity.java
@@ -1,25 +1,57 @@
 package com.example.sm472.assignmenteight;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-public class GameActivity extends  AppCompatActivity {
+public class GameActivity extends AppCompatActivity{
+
+    Player player ;
+    class GraphicsView extends View{
+
+        public GraphicsView(Context context)
+        {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            player.Draw(canvas);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        //Hide actionbar
+
         ActionBar actionBar = (ActionBar) getSupportActionBar();
         actionBar.hide();
 
         //set fullscreen sticky immersive
         int uioptions = View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         getWindow().getDecorView().setSystemUiVisibility(uioptions);
+
+
+
+
+
+        player = new Player(100,100,50,getColor(R.color.colorPrimary));
+        GraphicsView graphicsview = new GraphicsView(this);
+        ConstraintLayout c = (ConstraintLayout)findViewById(R.id.gamelayout);
+        c.addView(graphicsview);
+
     }
+
+
 }
--- app/src/main/java/com/example/sm472/assignmenteight/MainActivity.java
+++ app/src/main/java/com/example/sm472/assignmenteight/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.sm472.assignmenteight;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Hide actionbar
+//
+//        //Hide actionbar
         ActionBar actionBar = (ActionBar) getSupportActionBar();
         actionBar.hide();
 
@@ -21,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         getWindow().getDecorView().setSystemUiVisibility(uioptions);
+
+
+
     }
+
+
+    public void OnClickplay(View v) {
+
+        Toast.makeText(this,"CLICKED",Toast.LENGTH_SHORT).show();
+        intent = new Intent(this,GameActivity.class);
+        startActivity(intent);
+    }
+
 }
